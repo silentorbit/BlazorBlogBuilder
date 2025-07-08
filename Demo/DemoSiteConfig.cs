e38c909a83895506875571deb690158d02e5cf7c
Peter Hultqvist <hultqvist@silentorbit.com>
2025-07-08 20:35:09 +0200

    Add NuGet SilentOrbit.StaticOnline
    and apply modifications to Demo project.

---
+++ Demo/DemoSiteConfig.cs
@@ -0,0 +1,50 @@
+﻿using Demo.Components;
+using SilentOrbit.StaticOnline.Config;
+
+namespace Demo;
+
+public class DemoSiteConfig : SiteConfig<App>
+{
+    public DemoSiteConfig() : this(localhost: false) { }
+
+    public DemoSiteConfig(bool localhost)
+    {
+        Title = "Demo";
+        Description = "This is a Demo";
+        if (localhost)
+        {
+            //Live testing on root
+            BaseURL = "https://localhost:7127/";
+        }
+        else
+        {
+            //Build static files to be served in the subdirectory
+            BaseURL = "https://www.silentorbit.com/static-online";
+        }
+        Favicon = new()
+        {
+            URL = "https://www.silentorbit.com/favicon.png",
+            MimeType = "image/png"
+        };
+        CommentEmail = "demo@silentorbit.com";
+        DefaultRobots.NoTranslate = true;
+        DefaultRobots.Sitemap = true;
+        Author.Name = "Silent Orbit";
+
+        TopMenu.Add(new()
+        {
+            Title = "Blog",
+            Link = BaseURL.Append("/")
+        });
+        TopMenu.Add(new()
+        {
+            Title = "Tags",
+            Link = BaseURL.Append("/tags")
+        });
+        TopMenu.Add(new()
+        {
+            Title = "Count 10",
+            Link = BaseURL.Append("/mod10")
+        });
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,6 @@
 using Demo.Components;
+using SilentOrbit.Disk;
+using SilentOrbit.StaticOnline;
 
 namespace Demo;
 
@@ -6,10 +8,31 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        //Static Online: Build entire site
+        if (string.Join(",", args) == "build")
+        {
+            var workingDir = DirPath.GetCurrentDirectory();
+
+            var config = new DemoSiteConfig
+            {
+                TargetDir = workingDir.Parent.CombineDir("generated"),
+                WwwRoot = workingDir.CombineDir("wwwroot"),
+            };
+
+            config.Build().Wait();
+            return;
+        }
+
+        //Static Online: Run before staring web server
+        new DemoSiteConfig(localhost: true)
+            .Init();
+
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
         builder.Services.AddRazorComponents();
+        //Static Online: inject SiteBuilder and SitePage
+        builder.Services.AddStaticOnline();
 
         var app = builder.Build();
 
@@ -24,6 +47,9 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseAntiforgery();
+
+        //Static Online: Serve generated feeds and sitemaps
+        app.MapStaticOnline();
 
         app.MapStaticAssets();
         app.MapRazorComponents<App>();
