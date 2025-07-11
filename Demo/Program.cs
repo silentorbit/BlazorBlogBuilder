d356901318df80d1f8e5fb643b6b1838ca590fe8
Peter Hultqvist <hultqvist@silentorbit.com>
2025-07-11 16:30:48 +0200

    Exclude Error page from generated content

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,15 +1,48 @@
-using Demo;
 using Demo.Components;
 using SilentOrbit.Disk;
 using SilentOrbit.StaticOnline;
-using SilentOrbit.StaticOnline.Building;
-using System.Diagnostics;
+using SilentOrbit.StaticOnline.Config;
 
 //StaticOnline
-var config = new DemoSiteConfig();
-//StaticOnline: For building from a GitHub Action
+var config = new SiteConfig<App>
+{
+    Title = "Demo",
+    Description = "This is a Demo",
+    //Build static files to be served in the subdirectory
+    BaseURL = "https://www.silentorbit.com/static-online",
+    Favicon = new()
+    {
+        URL = "https://www.silentorbit.com/favicon.png",
+        MimeType = "image/png"
+    },
+    CommentEmail = "demo@silentorbit.com",
+    DefaultRobots = new()
+    {
+        NoTranslate = true,
+        Sitemap = true,
+    },
+    Author = new()
+    {
+        Name = "Silent Orbit",
+    },
+};
+config.TopMenu = new() {
+    new() { Title = "Blog",     Link = config.BaseURL.Append("/")    },
+    new() { Title = "Tags",     Link = config.BaseURL.Append("/tags")    },
+    new() { Title = "Count 10", Link = config.BaseURL.Append("/mod10")    }
+};
+
+//For building from a GitHub Action
 if (args.FirstOrDefault() == "build" && args.Length == 2)
+{
     config.Target = DirPath.GetCurrentDirectory().CombineDir(args[1]);
+    config.ExitAfterBuildComplete = true;
+}
+else
+{
+    //Generation target and options
+    config.Target = new DirPath("../generated/");
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,11 +65,11 @@ app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
-//Static Online: Serve generated feeds and sitemaps
-app.BuildStaticOnline(config);
-
 app.MapStaticAssets();
 
 app.MapRazorComponents<App>();
+
+//Static Online: Serve generated feeds and sitemaps
+app.BuildStaticOnline(config);
 
 app.Run();
