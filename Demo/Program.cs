bd7c930b4423651bf61821f3be58f004d350640e
Peter Hultqvist <hultqvist@silentorbit.com>
2025-07-13 10:29:02 +0200

    New Console logging

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -58,16 +58,6 @@ var app = builder.Build();
 
 app.UsePathBase(config.BaseURL.Href.TrimEnd('/') + '/');
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
